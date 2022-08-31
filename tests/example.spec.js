3d623050536c151ee0907f61905ef18f6519efe1
t5software <58944328+t5software@users.noreply.github.com>
2022-08-31 11:58:27 -0700

    fix count and add go back #4

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -11,21 +11,26 @@ test('check most stars from forks', async ({ page }) => {
   result.startTime = Date.now();
   console.log('startTime - ' + result.startTime);
   
-  const allForks = (await page.locator(selector)).count();
-  console.log('allForks - ' + allForks);
+  const allForks = await page.locator(selector);
+  const total = await allForks.count();
+  console.log('allForks - ' + total);
   
-  for (let i = 1; i < allForks; ++i) {
+  for (let i = 1; i < total; ++i) {
+    
     rows = await page.locator(selector);
     console.log('fork - ' + i);
     await rows.nth(i).click();
+    
     let stars = await page.locator('#repo-stars-counter-star').innerText();
+    
     if(stars > result.max) {
       result.max = stars;
       result.winner = page.url();
       console.log('new max - ' + JSON.stringify(result));
     }
     
-    console.log('winner - ' + JSON.stringify(result, null, 2));
+    await page.goBack();
   }
-
+  
+  console.log('winner - ' + JSON.stringify(result, null, 2));
 });
