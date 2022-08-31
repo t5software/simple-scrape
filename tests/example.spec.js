559eefdc6ccbab99f0933bbdd95355a07f76de33
t5software <58944328+t5software@users.noreply.github.com>
2022-08-31 13:27:50 -0700

    try again #4

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,6 +2,9 @@
 const { test, expect } = require('@playwright/test');
 
 test('check most stars from forks', async ({ page }) => {
+  console.log('setTimeout - ' + process.env.TIMEOUT);
+  test.setTimeout(process.env.TIMEOUT);
+  
   await page.goto('https://github.com/NomicFoundation/hardhat-boilerplate/network/members');
   
   const selector = '.repo > a:last-child';
@@ -11,25 +14,26 @@ test('check most stars from forks', async ({ page }) => {
   result.startTime = Date.now();
   console.log('startTime - ' + result.startTime);
   
-  const allForks = await page.locator(selector);
-  const total = await allForks.count();
-  console.log('allForks - ' + total);
+  const elements = page.locator(selector);
+  const allForks = await elements.evaluateAll((forks) => forks.map(e => e.href));
+  console.log('allForks - ' + allForks.length);
   
-  for (let i = 2; i < total; ++i) {
+  for (let i = 1; i < allForks.length; ++i) {
     
-    const rows = await page.locator(selector);
     console.log('fork - ' + i);
-    await rows.nth(i).click();
     
-    let stars = await page.locator('#repo-stars-counter-star').innerText();
+    await page.goto(allForks[i], { waitUntil: 'commit'});
+    
+    // await page.waitForNavigation({ waitUntil: 'commit'});
+    
+    const element = await page.waitForSelector('#repo-stars-counter-star');
+    let stars = await element.innerText();
     
     if(stars > result.max) {
       result.max = stars;
-      result.winner = page.url();
+      result.winner = await page.url();
       console.log('new max - ' + JSON.stringify(result));
     }
-    
-    await page.goBack();
   }
   
   console.log('winner - ' + JSON.stringify(result, null, 2));
